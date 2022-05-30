e7f2358862264055e60842673b9513f06465a173
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-05-30 22:23:48 +0300

    1981 edited

--- lc_1981.java
+++ lc_1981.java
@@ -1,19 +1,17 @@
 
 public class lc_1981 {
     static int subtractProductAndSum(int n) {
-        if (n<10) return 0;
         int mult = 1,
             sum = 0;
-        while (n > 9) {
-            int elem = n % 10;
-            mult *= elem;
-            sum += elem; 
+        while (n > 0) {
+            int lastDigit = n % 10;
+            mult *= lastDigit;
+            sum += lastDigit; 
             n /= 10;                
         }
         mult *= n;
         sum += n;
-        int result = mult - sum;
-    return result;
+    return mult - sum;
     }
     public static void main(String[] args) {
         // System.out.println(subtractProductAndSum(239)); 
