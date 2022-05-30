27adb2136981ea22f7e256d4bbae9ccbf95c08f8
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-05-30 16:34:36 +0300

    1981 added

---
+++ lc_1981.java
@@ -0,0 +1,22 @@
+package gb;
+
+public class lc_1981 {
+    static int subtractProductAndSum(int n) {
+        if (n<10) return 0;
+        int mult = 1,
+            sum = 0;
+        while (n > 9) {
+            int elem = n % 10;
+            mult *= elem;
+            sum += elem; 
+            n /= 10;                
+        }
+        mult *= n;
+        sum += n;
+        int result = mult - sum;
+    return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(subtractProductAndSum(239)); 
+    }}
+
