2239bf9a376d9508acf7e7b75670c6c0c4ea2eee
ReltTull <92668493+ReltTull@users.noreply.github.com>
2022-05-30 16:32:15 +0300

    1920 added

---
+++ lc_1920.java
@@ -0,0 +1,19 @@
+package gb;
+
+public class lc_1920 {
+    static int[] swappedMeaningForIndex(int[] inputArray) {
+        int[] result = new int [inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            result[i] = inputArray[inputArray[i]];
+        }
+        return result;
+        }
+        static void printArray(int[] input) {
+            for (int i = 0; i < input.length; i++) {
+                System.out.println(input[i]);
+            }
+        }
+        
+    public static void main(String[] args) {
+        printArray(swappedMeaningForIndex(new int[]{3, 0, 2, 1}));
+}}
